89796553ccecae6c56df124864906a3d4781f34e
Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-11-21 19:54:45 +0000

    Clase 21.11.2024

---
+++ src/app/components/privacy/privacy.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      privacy works!
+    </p>
+  `,
+  styles: ``
+})
+export class PrivacyComponent {
+
+}
